92d726c70620bb1b7c33338f6f451374e7a6d0dc
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-03-25 14:55:43 +0100

    Deleted old header file with solver parameters, fixed failing io test

--- tests/io_tests_fluid.py
+++ tests/io_tests_fluid.py
@@ -12,7 +12,6 @@ orig.defaultParams()
 orig.initRandomGridPos()
 orig.initFluid()
 orig.initTemporal(current=0.0, total=0.0)
-orig.g[2] = 0.0
 orig.time_total=2.0*orig.time_dt
 orig.time_file_dt = orig.time_dt
 orig.writebin(verbose=False)
@@ -34,6 +33,10 @@ cuda = sphere.sim(fluid=True)
 cuda.readbin("../output/" + orig.sid + ".output00001.bin", verbose=False)
 cuda.time_current = orig.time_current
 cuda.time_step_count = orig.time_step_count
+compareNumpyArraysClose(orig.v_f, cuda.v_f, "cuda.v_f:", tolerance=1e-5)
+cuda.v_f = orig.v_f
+compareNumpyArraysClose(orig.p_f, cuda.p_f, "cuda.p_f:", tolerance=0.1)
+cuda.p_f = orig.p_f
 compare(orig, cuda, "CUDA IO:  ")
 
 # Remove temporary files
