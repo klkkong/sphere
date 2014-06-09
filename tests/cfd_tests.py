696e6181e56f183babb7c39e7f390c2f6e9519b5
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-06-09 15:05:16 +0200

    cfd_tests results verified

--- tests/cfd_tests.py
+++ tests/cfd_tests.py
@@ -43,7 +43,7 @@ orig.run(verbose=False)
 #orig.run(verbose=True)
 py.readlast(verbose = False)
 ideal_grad_p_z = numpy.linspace(orig.p_f[0,0,0], orig.p_f[0,0,-1], orig.num[2])
-py.writeVTKall()
+#py.writeVTKall()
 compareNumpyArraysClose(numpy.zeros((1,orig.num[2])),\
         ideal_grad_p_z - py.p_f[0,0,:],\
         "Pressure gradient:\t", tolerance=1.0e-1)
