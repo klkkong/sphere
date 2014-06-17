86fdfad131a636d5ce80131892b66d157250f0c5
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-06-17 13:46:51 +0200

    neumann test passes with set2

--- tests/cfd_tests_neumann.py
+++ tests/cfd_tests_neumann.py
@@ -15,7 +15,7 @@ cleanup(orig)
 orig.defaultParams(mu_s = 0.4, mu_d = 0.4)
 orig.defineWorldBoundaries([0.4, 0.4, 1], dx = 0.1)
 orig.initFluid(mu = 8.9e-4)
-orig.initTemporal(total = 1.0, file_dt = 0.05, dt = 1.0e-4)
+orig.initTemporal(total = 0.5, file_dt = 0.05, dt = 1.0e-4)
 py = sphere.sim(sid = orig.sid, fluid = True)
 orig.bc_bot[0] = 1      # No-flow BC at bottom (Neumann)
 #orig.run(dry=True)
@@ -34,7 +34,6 @@ else:
     print("Flow field:\t\t" + failed())
     raise Exception("Failed")
 
-"""
 print('''# Neumann bottom, Dirichlet top BC.
 # Gravity, pressure gradients => transient flow''')
 orig = sphere.sim("neumann", fluid = True)
@@ -47,7 +46,7 @@ orig.g[2] = -10.0
 orig.bc_bot[0] = 1      # No-flow BC at bottom (Neumann)
 #orig.run(dry=True)
 orig.run(verbose=False)
-#orig.writeVTKall()
+orig.writeVTKall()
 py.readlast(verbose = False)
 ideal_grad_p_z = numpy.linspace(
         orig.p_f[0,0,0] + orig.L[2]*orig.rho_f*numpy.abs(orig.g[2]),
@@ -62,5 +61,4 @@ else:
     print("Flow field:\t\t" + failed())
     raise Exception("Failed")
 
-#orig.cleanup()
-"""
+orig.cleanup()
