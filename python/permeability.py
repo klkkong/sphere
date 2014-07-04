7f0969d3d4369bfee18b6723b4f1440cf0f42536
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-07-04 15:00:42 +0200

    fixed permeability test

--- python/permeability.py
+++ python/permeability.py
@@ -5,22 +5,33 @@ import numpy
 
 for dp in [1.0e3, 2.0e3, 4.0e3, 10.0e3, 20.0e3, 40.0e3, 80.0e3]:
     # Read initial configuration
-    sim.sid = 'diffusivity-relax'
+    sim = sphere.sim('diffusivity-relax')
     sim.readlast()
 
     sim.sid = 'permeability-dp=' + str(dp)
     sim.cleanup()
 
-    sim.g[2] = -9.81
+    sim.g[2] = 0.0
     sim.nw[0] = 0
     sim.initGrid()
     sim.zeroKinematics()
-    sim.initFluid(mu = 17.87e-4, p = 1.0e5, hydrostatic=True)  # mu = water at 0 deg C
+    sim.initFluid(mu = 17.87e-4, p = 1.0e5, hydrostatic=True)
+
+    # Initialize to linear hydraulic gradient
+    p_bottom = 10.0
+    p_top = p_bottom + dp
+    dz = sim.L[2]/sim.num[2]
+    for iz in range(sim.num[2]-1):
+        #z = dz*iz + 0.5*dz # cell-center z-coordinate
+        z = dz*iz
+        sim.p_f[:,:,iz] = p_bottom + dp/sim.L[2] * z
+
     sim.setFluidTopFixedPressure()
-    sim.p_f[:,:,-1] = dp
-    #sim.setDEMstepsPerCFDstep(100)
+    sim.setFluidBottomFixedPressure()
+    sim.p_f[:,:,-1] = p_top
     sim.setDEMstepsPerCFDstep(10)
     sim.initTemporal(total = 2.0, file_dt = 0.01, epsilon=0.07)
+
     sim.run(dry=True)
     sim.run()
     sim.writeVTKall()
