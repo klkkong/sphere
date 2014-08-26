c6a424a797d75aa74e401d8ba05ad6cee526de78
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-08-26 10:25:03 +0200

    added diagnostics script

---
+++ run_diagnostics.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+OUTFILE=diagnostics.txt
+
+function report {
+    echo "\n### $@ ###" >> $OUTFILE
+    $@ >> $OUTFILE 2>&1
+}
+
+report ls -lhR .
+report git show
+report git status
+report lspci
+report uname -a
+report cmake --version
+report cmake .
+report make
+report make test
+report cat Testing/Temporary/LastTestsFailed.log
+report cat Testing/Temporary/LastTest.log
+
+echo "### Diagnostics complete ###"
+echo "Report bugs and unusual behavior to anders.damsgaard@geo.au.dk"
