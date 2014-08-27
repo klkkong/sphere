1464c92fe0e7fffaf127a24b567a9fb224370ca4
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-08-27 10:18:06 +0200

    increase total time to 10s

--- python/consolidation-curves.py
+++ python/consolidation-curves.py
@@ -33,7 +33,7 @@ sim.setFluidBottomNoFlow()
 sim.setFluidTopFixedPressure()
 sim.setDEMstepsPerCFDstep(10)
 sim.setMaxIterations(2e5)
-sim.initTemporal(total = 5.0, file_dt = 0.01, epsilon=0.07)
+sim.initTemporal(total = 10.0, file_dt = 0.01, epsilon=0.07)
 sim.c_grad_p[0] = c_grad_p
 sim.c_phi[0] = c_phi
 
