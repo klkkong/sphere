5702a349deb400506e747ef13fe3b2f18a67e593
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-08-29 15:09:43 +0200

    rename sid during collapse

--- python/capillary-cohesion.py
+++ python/capillary-cohesion.py
@@ -16,10 +16,30 @@ import sys
 device = sys.argv[1]
 cohesion = sys.argv[2]
 
-sim = sphere.sim('cap-cohesion=' + str(cohesion), np=2000)
-sim.defaultParams(capillaryCohesion = cohesion)
+# Create packing
+sim = sphere.sim('cap-cohesion=' + str(cohesion) + '-init', np=2000)
+sim.mu_s[0] = 0.0
+sim.mu_d[0] = 0.0
 sim.generateRadii(psd='logn', radius_mean=1.0e-3, radius_variance=1.0e-4)
 sim.contactModel(1)
 sim.initRandomGridPos([12, 12, 10000])
+sim.initTemporal(5.0, file_dt=0.01, epsilon=0.07)
+sim.g[2] = -10.0
+sim.run()
+
+sim.readlast()
+sim.sid = 'cap-cohesion=' + str(cohesion)
+sim.defaultParams(capillaryCohesion=cohesion)
+sim.adjustUpperWall()
+init_lx = sim.L[0]
+init_ly = sim.L[1]
+sim.L[0] *= 5
+sim.L[1] *= 5
+sim.num[0] *= 5
+sim.num[1] *= 5
+sim.x[:,0] += 0.5*sim.L[0] - 0.5*init_lx
+sim.x[:,1] += 0.5*sim.L[1] - 0.5*init_ly
 sim.initTemporal(2.0, file_dt=0.01, epsilon=0.07)
 sim.run()
+
+sim.writeVTKall()
