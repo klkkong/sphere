fb6705d3af67c7a44b029e40c1c819013a9b58a9
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-09-01 09:09:32 +0200

    run gravity-free simulation on starting grid

--- python/capillary-cohesion.py
+++ python/capillary-cohesion.py
@@ -21,39 +21,35 @@ cohesion = sys.argv[2]
 gravity = sys.argv[3]
 
 # Create packing
-sim = sphere.sim('cap-cohesion=' + str(cohesion) + '-init', np=2000)
+sim = sphere.sim('cap-cohesion=' + str(cohesion) + '-init-grav=' \
+        + str(gravity), np=2000)
 sim.mu_s[0] = 0.0
 sim.mu_d[0] = 0.0
+sim.k_n[0] = 1.0e7
+sim.k_t[0] = 1.0e7
 sim.generateRadii(psd='uni', radius_mean=1.0e-3, radius_variance=1.0e-4)
 sim.contactModel(1)
 sim.initRandomGridPos([12, 12, 10000])
 sim.initTemporal(5.0, file_dt=0.01, epsilon=0.07)
-sim.g[2] = -10.0
+if gravity == 1:
+    sim.g[2] = -10.0
 sim.run()
 
-sim.readlast()
-sim.sid = 'cap-cohesion=' + str(cohesion)
-sim.defaultParams(capillaryCohesion=cohesion)
-sim.adjustUpperWall()
-init_lx = sim.L[0]
-init_ly = sim.L[1]
-sim.L[0] *= 5
-sim.L[1] *= 5
-sim.num[0] *= 5
-sim.num[1] *= 5
-sim.x[:,0] += 0.5*sim.L[0] - 0.5*init_lx
-sim.x[:,1] += 0.5*sim.L[1] - 0.5*init_ly
+if gravity == 1:
+    sim.readlast()
+    sim.sid = 'cap-cohesion=' + str(cohesion)
+    sim.defaultParams(capillaryCohesion=cohesion)
+    sim.adjustUpperWall()
+    init_lx = sim.L[0]
+    init_ly = sim.L[1]
+    sim.L[0] *= 5
+    sim.L[1] *= 5
+    sim.num[0] *= 5
+    sim.num[1] *= 5
+    sim.x[:,0] += 0.5*sim.L[0] - 0.5*init_lx
+    sim.x[:,1] += 0.5*sim.L[1] - 0.5*init_ly
 
-if gravity == 0:
-    init_lz = sim.L[2]
-    sim.L[2] *= 5
-    sim.num[2] *= 5
-    sim.w_x[0] = sim.L[2]
-    sim.sid = sim.sid + '-nograv'
-    sim.x[:,2] += 0.5*sim.L[2] - 0.5*init_lz
-    sim.g[2] = 0.0
-
-sim.initTemporal(2.0, file_dt=0.01, epsilon=0.07)
-sim.run()
+    sim.initTemporal(2.0, file_dt=0.01, epsilon=0.07)
+    sim.run()
 
 sim.writeVTKall()
