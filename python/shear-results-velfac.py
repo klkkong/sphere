4f39ae81a66b0fd3f6d57da1ced96d209187fb53
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-10-27 11:09:27 +0100

    added script to investigate rate dependence

--- python/shear-results-velfac.py
+++ python/shear-results-velfac.py
@@ -22,8 +22,15 @@ zflow = True
 #sigma0 = 10.0e3
 sigma0 = float(sys.argv[1])
 #cvals = [1.0, 0.1]
-cvals = [1.0, 0.1, 0.01]
+#cvals = [1.0, 0.1, 0.01]
 #cvals = [1.0]
+fluid = False
+if int(sys.argv[2]) == 1:
+    print('fluid = True')
+    fluid = True
+    c_v = float(sys.argv[3])
+    c_a = float(sys.argv[4])
+velfacvals = [1.0, 0.5, 2.0]
 
 # return a smoothed version of in. The returned array is smaller than the
 # original input array
@@ -107,41 +114,19 @@ f_n_mean = [[], [], [], []]
 f_n_max  = [[], [], [], []]
 v_f_z_mean  = [[], [], [], []]
 
-fluid=True
+for c in numpy.arange(0,len(velfacvals)):
 
-# dry shear
-#sid = 'shear-sigma0=' + sys.argv[1] + '-hw'
-sid = 'halfshear-sigma0=' + sys.argv[1] + '-shear'
-sim = sphere.sim(sid)
-sim.readlast(verbose=False)
-sim.visualize('shear')
-shear_strain[0] = sim.shear_strain
-#shear_strain[0] = numpy.arange(sim.status()+1)
-friction[0] = sim.tau/sim.sigma_eff
-if smoothed_results:
-    friction_smooth[0] = smooth(friction[0], smooth_window)
-dilation[0] = sim.dilation
-
-if contact_forces:
-    f_n_mean[0] = numpy.zeros_like(shear_strain[0])
-    f_n_max[0]  = numpy.zeros_like(shear_strain[0])
-    for i in numpy.arange(sim.status()):
-        sim.readstep(i, verbose=False)
-        sim.findNormalForces()
-        f_n_mean[0][i] = numpy.mean(sim.f_n_magn)
-        f_n_max[0][i]  = numpy.max(sim.f_n_magn)
-
-# wet shear
-c = 1
-for c in numpy.arange(1,len(cvals)+1):
-    c_v = cvals[c-1]
+    velfac = velfacvals[c]
 
     #sid = 'shear-sigma0=' + str(sigma0) + '-c_phi=' + \
-                    #str(c_phi) + '-c_v=' + str(c_v) + \
+                    #str(c_phi) + '-c_grad_p=' + str(c_grad_p) + \
                     #'-hi_mu-lo_visc-hw'
-    #sid = 'halfshear-sigma0=' + str(sigma0) + '-c=' + str(c_v) + '-shear'
-    sid = 'halfshear-sigma0=' + str(sigma0) + '-c_v=' + str(c_v) +\
-            '-c_a=0.0-velfac=1.0-shear'
+    if fluid:
+        sid = 'halfshear-sigma0=' + str(sigma0) + '-c_v=' + str(c_v) + \
+                '-c_a=' + str(c_a) + '-velfac=' + str(velfac) + '-shear'
+    else:
+        sid = 'halfshear-sigma0=' + str(sigma0) + '-velfac=' + str(velfac) + \
+                '-shear'
     if os.path.isfile('../output/' + sid + '.status.dat'):
 
         sim = sphere.sim(sid, fluid=fluid)
@@ -168,7 +153,7 @@ for c in numpy.arange(1,len(cvals)+1):
             f_n_mean[c] = numpy.zeros_like(shear_strain[c])
             f_n_max[c]  = numpy.zeros_like(shear_strain[c])
             for i in numpy.arange(sim.status()):
-                if pressures:
+                if pressures and fluid:
                     sim.readstep(i, verbose=False)
                     iz_top = int(sim.w_x[0]/(sim.L[2]/sim.num[2]))-1
                     p_mean[c][i] = numpy.mean(sim.p_f[:,:,0:iz_top])/1000
@@ -180,7 +165,7 @@ for c in numpy.arange(1,len(cvals)+1):
                     f_n_mean[c][i] = numpy.mean(sim.f_n_magn)
                     f_n_max[c][i]  = numpy.max(sim.f_n_magn)
 
-        if zflow:
+        if zflow and fluid:
             v_f_z_mean[c] = numpy.zeros_like(shear_strain[c])
             for i in numpy.arange(sim.status()):
                     v_f_z_mean[c][i] = numpy.mean(sim.v_f[:,:,:,2])
@@ -211,32 +196,23 @@ ax2 = plt.subplot(312, sharex=ax1)
 ax3 = plt.subplot(313, sharex=ax1)
 #ax3 = plt.subplot(413, sharex=ax1)
 #ax4 = plt.subplot(414, sharex=ax1)
-alpha = 0.5
-if smoothed_results:
-    x1.plot(shear_strain[0], friction_smooth[0], label='dry', linewidth=1,
-            alpha=0.5)
-else:
-    ax1.plot(shear_strain[0], friction[0], label='dry', linewidth=1, alpha=0.5)
-ax2.plot(shear_strain[0], dilation[0], label='dry', linewidth=1)
-#ax4.plot(shear_strain[0], f_n_mean[0], '-', label='dry', color='blue')
-#ax4.plot(shear_strain[0], f_n_max[0], '--', color='blue')
 
 color = ['b','g','r','c']
-for c in numpy.arange(1,len(cvals)+1):
+for c in numpy.arange(len(velfacvals)):
 
     if smoothed_results:
         ax1.plot(shear_strain[c][1:], friction_smooth[c][1:], \
-                label='$c$ = %.2f' % (cvals[c-1]), linewidth=1, alpha=0.5)
+                label='$\\dot{\\gamma}$ = %.2f' % (velfacvals[c]), linewidth=1, alpha=0.5)
     else:
         ax1.plot(shear_strain[c][1:], friction[c][1:], \
-                label='$c$ = %.2f' % (cvals[c-1]), linewidth=1, alpha=0.5)
+                label='$\\dot{\\gamma}$ = %.2f' % (velfacvals[c]), linewidth=1, alpha=0.5)
 
     ax2.plot(shear_strain[c][1:], dilation[c][1:], \
-            label='$c$ = %.2f' % (cvals[c-1]), linewidth=1)
+            label='$\\dot{\\gamma}$ = %.2f' % (velfacvals[c]), linewidth=1, alpha=0.5)
 
     if zflow:
         ax3.plot(shear_strain[c][1:], v_f_z_mean[c][1:],
-            label='$c$ = %.2f' % (cvals[c-1]), linewidth=1)
+            label='$\\dot{\\gamma}$ = %.2f' % (velfacvals[c]), linewidth=1, alpha=0.5)
 
 
     '''
