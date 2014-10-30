7a7cc64924f23adf01350570147feaf6ecf6e5e3
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-10-30 19:41:38 +0100

    dphi should be zero in empty cells

--- src/darcy.cuh
+++ src/darcy.cuh
@@ -406,7 +406,7 @@ __global__ void findDarcyPorosities(
             //dev_darcy_phi[cellidx]  = phi;
             //dev_darcy_dphi[cellidx] = dphi;
             dev_darcy_phi[cellidx]  = 0.9;
-            dev_darcy_dphi[cellidx] = 0.1;
+            dev_darcy_dphi[cellidx] = 0.0;
 
             //dev_darcy_vp_avg[cellidx] = MAKE_FLOAT3(0.0, 0.0, 0.0);
             //dev_darcy_d_avg[cellidx]  = 0.0;
