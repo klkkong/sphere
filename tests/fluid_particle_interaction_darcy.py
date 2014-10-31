0d767fc9b445e15a36e0896d466596b36cf9dba5
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-10-31 14:43:00 +0100

    debugging fluid-particle interaction with output from updateDarcySolution

---
+++ tests/fluid_particle_interaction_darcy.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import sphere
+from pytestutils import *
+
+sim = sphere.sim('fluid_particle_interaction', fluid=True)
+sim.cleanup()
+
+sim.defineWorldBoundaries([1.0, 1.0, 1.0], dx = 0.1)
+sim.initFluid(cfd_solver = 1)
+
+
+# No gravity, pressure gradient enforced by Dirichlet boundaries.
+# The particle should be sucked towards the low pressure
+print('# Test 1: Test pressure gradient force')
+sim.p_f[:,:,0]  = 1.0
+sim.p_f[:,:,-1] = 1.1
+sim.addParticle([0.5, 0.5, 0.5], 0.05)
+sim.initTemporal(total=0.01, file_dt=0.001)
+
+sim.run(verbose=True)
+#sim.run(dry=True)
+#sim.run(cudamemcheck=True)
+sim.writeVTKall()
+
+sim.readlast()
+test(sim.vel[0,2] > 0.0, 'Particle velocity:')
+
+sim.cleanup()
