182947ade73c6a26f13c6d234d17ad71844ca2dd
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-11-05 13:27:50 +0100

    add standard particle cube

--- tests/cfd_tests_darcy_particles.py
+++ tests/cfd_tests_darcy_particles.py
@@ -3,7 +3,7 @@ from pytestutils import *
 import sphere
 import numpy
 
-
+'''
 print("### Steady state, no gravity, no forcing, Dirichlet+Dirichlet BCs")
 orig = sphere.sim('darcy_particles', np = 1000)
 orig.cleanup()
@@ -112,6 +112,49 @@ else:
     print(numpy.mean(py.v_f))
     print(numpy.max(py.v_f))
     raise Exception("Failed")
+'''
 
+
+print("### Fluidization test: Transient, gravity, Dirichlet+Dirichlet BCs")
+#orig = sphere.sim('diffusivity-relax', fluid=False)
+orig = sphere.sim('cube-init', fluid=False)
+orig.readlast()
+orig.id('darcy_fluidization')
+orig.cleanup()
+orig.initTemporal(total=0.005, file_dt=0.001)
+orig.initFluid(cfd_solver=1)
+orig.g[2] = -10.0
+
+fluidize_pressure = -(orig.rho - orig.rho_f) \
+        *(1.0 - numpy.mean(orig.phi))*numpy.abs(orig.g[2])
+
+fluid_pressure_gradient = numpy.array([0.1, 0.9, 1.1, 2.0])
+
+for i in numpy.arange(fluid_pressure_gradient.size):
+
+    # set pressure gradient
+    dpdz = fluid_pressure_gradient[i] * fluidize_pressure
+    dp = dpdz * orig.L[2]
+    base_p = 0.0
+    orig.p_f[:,:,0] = base_p
+    orig.p_f[:,:,-1] = base_p + dp
+
+    orig.run(verbose=False)
+    #orig.writeVTKall()
+    py = sphere.sim(sid = orig.sid, fluid = True)
+    py.readlast(verbose=False)
+
+    print('Mean particle velocity: '
+            + str(numpy.mean(py.vel[:,0])) + ', '
+            + str(numpy.mean(py.vel[:,1])) + ', '
+            + str(numpy.mean(py.vel[:,2])) + ' m/s')
+
+    z_vel_threshold = 0.001
+    if fluid_pressure_gradient[i] < 1.0:
+        test('Fluidization (' + str(i) + '):\t',
+                numpy.mean(py.vel[:,2]) < z_vel_threshold)
+    elif fluid_pressure_gradient[i] > 1.0:
+        test('Fluidization (' + str(i) + '):\t',
+                numpy.mean(py.vel[:,2]) > z_vel_threshold)
 
 
