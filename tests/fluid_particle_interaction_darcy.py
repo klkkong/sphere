04f0dda8bcbf205fd4468c4f80a324dbbbf070f8
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-11-10 13:27:56 +0100

    update for default values, quiet run

--- tests/fluid_particle_interaction_darcy.py
+++ tests/fluid_particle_interaction_darcy.py
@@ -12,15 +12,14 @@ sim.initFluid(cfd_solver = 1)
 # No gravity, pressure gradient enforced by Dirichlet boundaries.
 # The particle should be sucked towards the low pressure
 print('# Test 1: Test pressure gradient force')
-sim.p_f[:,:,0]  = 1.0
-sim.p_f[:,:,-1] = 1.1
+sim.p_f[:,:,-1] = 1.0
 sim.addParticle([0.5, 0.5, 0.5], 0.01)
 sim.initTemporal(total=0.001, file_dt=0.0001)
 #sim.time_file_dt[0] = sim.time_dt[0]
 #sim.time_total[0] = sim.time_dt[0]
 
-#sim.run(verbose=False)
-sim.run()
+sim.run(verbose=False)
+#sim.run()
 #sim.run(dry=True)
 #sim.run(cudamemcheck=True)
 #sim.writeVTKall()
