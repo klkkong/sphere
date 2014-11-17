b03a72b12c3a7b36d360962ee1f3ad6415505475
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-11-17 10:47:43 +0100

    add velocity scaling factor

--- python/halfshear-darcy-starter.py
+++ python/halfshear-darcy-starter.py
@@ -4,7 +4,7 @@ import numpy
 import sys
 
 # launch with:
-# $ ipython halfshear-darcy-starter.py <device> <fluid> <c_phi> <k_c> <sigma_0> <mu>
+# $ ipython halfshear-darcy-starter.py <device> <fluid> <c_phi> <k_c> <sigma_0> <mu> <velfac>
 
 device = int(sys.argv[1])
 wet = int(sys.argv[2])
@@ -12,6 +12,7 @@ c_phi = float(sys.argv[3])
 k_c = float(sys.argv[4])
 sigma0 = float(sys.argv[5])
 mu = float(sys.argv[6])
+velfac = float(sys.argv[7])
 
 if wet == 1:
     fluid = True
@@ -25,9 +26,10 @@ sim.readlast()
 sim.fluid = fluid
 if fluid:
     sim.id('halfshear-darcy-sigma0=' + str(sigma0) + '-k_c=' + str(k_c) + \
-            '-mu=' + str(mu) + '-shear')
+            '-mu=' + str(mu) + '-velfac=' + str(velfac) + '-shear')
 else:
-    sim.id('halfshear-sigma0=' + str(sigma0) + '-shear')
+    sim.id('halfshear-sigma0=' + str(sigma0) + '-velfac=' + str(velfac) + \
+            '-shear')
 
 sim.checkerboardColors(nx=6,ny=3,nz=6)
 sim.cleanup()
@@ -35,7 +37,7 @@ sim.adjustUpperWall()
 sim.zeroKinematics()
 
 #sim.shear(0.0/20.0)
-sim.shear(1.0/20.0)
+sim.shear(1.0/20.0 * velfac)
 K_q_real = 36.4e9
 K_w_real =  2.2e9
 K_q_sim  = 1.16e9
@@ -68,7 +70,7 @@ sim.setDampingTangential(0.0)
 #sim.deleteAllParticles()
 #sim.fixvel[:] = -1.0
 
-sim.initTemporal(total = 20.0, file_dt = 0.01, epsilon=0.07)
+sim.initTemporal(total = 20.0/velfac, file_dt = 0.01/velfac, epsilon=0.07)
 #sim.time_dt[0] *= 1.0e-2
 #sim.initTemporal(total = 1.0e-4, file_dt = 1.0e-5, epsilon=0.07)
 
