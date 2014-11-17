694188f73c821ffd3d8d2374f833f864dba3a045
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-11-17 11:14:48 +0100

    refine top fluid gradient bc

--- src/darcy.cuh
+++ src/darcy.cuh
@@ -541,7 +541,7 @@ __global__ void findDarcyPressureForce(
         Float p_zp = dev_darcy_p[d_idx(i_x,i_y,i_z+1)];
 
         // Add Neumann BC at top wall
-        /*if (i_z >= wall0_iz - 1)
+        if (i_z >= wall0_iz - 1)
             p_zp = p;
             //p_zp = p_zn;*/
 
@@ -564,7 +564,7 @@ __global__ void findDarcyPressureForce(
                     //devC_params.g[2]);
 
         // Add Neumann BC at top wall
-        if (i_z >= wall0_iz - 1)
+        if (i_z >= wall0_iz)
             f_p.z = 0.0;
 
         /*printf("%d,%d,%d findPF:\n"
