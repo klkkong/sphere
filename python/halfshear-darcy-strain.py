4424ab8f7731d8c7bedbaeb93fbfffaaa6cf6b6d
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2015-04-14 20:02:29 +0200

    remove grid lines

--- python/halfshear-darcy-strain.py
+++ python/halfshear-darcy-strain.py
@@ -169,7 +169,7 @@ ax[0].spines['right'].set_visible(False)
 ax[0].get_xaxis().tick_bottom()
 ax[0].get_yaxis().tick_left()
 ax[0].get_xaxis().grid(False) # horizontal grid lines
-ax[0].get_yaxis().grid(True, linestyle='--', linewidth=0.5) # vertical grid lines
+#ax[0].get_yaxis().grid(True, linestyle='--', linewidth=0.5) # vertical grid lines
 
 # reverse legend order
 handles, labels = ax[0].get_legend_handles_labels()
